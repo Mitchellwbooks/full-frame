1c791189892cc7d28d283d42e588dfa719f8e706
mitchellwbooks <mitchellwbooks@gmail.com>
2023-06-24 16:22:12 -0500

    Stubbed Model Manager functions

--- src/server/core/ModelManager.py
+++ src/server/core/ModelManager.py
@@ -21,3 +21,14 @@ class ModelManager( Process ):
 
     def run( self ):
         pass
+
+    def async_run(self):
+        while True:
+            await self.load_inferencer_message()
+            await self.load_controller_message()
+
+    async def load_inferencer_message(self):
+        pass
+
+    async def load_controller_message(self):
+        pass
